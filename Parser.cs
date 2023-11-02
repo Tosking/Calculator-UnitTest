bbd6e06dadc826642dc984e1eaefb6dbbb5319ad
Tosking <tosking@duck.com>
2023-11-02 17:28:23 +0500

    added math priority

--- Parser.cs
+++ Parser.cs
@@ -33,8 +33,6 @@ public class Parser {
                         i++;
                         c = str[i].ToString();
                     }
-                    i++;
-                    c = str[i].ToString();
                     size++;
                 }
                 if(c != " "){
@@ -51,10 +49,24 @@ public class Parser {
         elements.Add(temp);
         size++;
         double result = 0;
+        int k = 1;
         if(size == 1){
             return elements[0];
         }
         for(int i = 1; i < size; i += 2){
+            while(k < size && k > 0){
+                if(elements[k] == "*" || elements[k] == "/"){
+                    i = k;
+                    k = 1;
+                    break;
+                }
+                k += 2;
+                if(k >= size){
+                    k = 0;
+                    i = 0;
+                    break;
+                }
+            }
             double loper;
             string sign = elements[i];
             double roper;
